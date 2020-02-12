aa2aa8e7b29f054916d93f871fa9deea3eb3cbd5
Pebaz <pebazium@gmail.com>
2020-02-12 12:08:00 -0500

    working cli file hasher

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ pbz-hash
 Fast file hasher in Rust using MeowHash.
 */
 
-use std::fs::OpenOptions;
+use std::env;
+use std::path::Path;
+use std::fs::File;
 use std::io::prelude::*;
 use digest::*;
 use meowhash::*;
@@ -12,13 +14,39 @@ use meowhash::*;
 const BUF_SIZE: usize = 1024 * 1024;
 
 fn main() {
-    let mut hasher = MeowHasher::new();
-    let mut file = OpenOptions::new().read(true).open(".gitignore").unwrap();    
-    let mut buffer = [0; BUF_SIZE];
+    let args = env::args();
 
-    while file.read(&mut buffer[..]).unwrap() > 0 {
-        hasher.input(&buffer[..]);
+    // Show help screen and exit if file is not given
+    if args.len() < 2 {
+        println!("pbz-hash - Hash large files efficiently.");
+        println!("    Usage: pbz-hash <filename>");
+        std::process::exit(0);
     }
 
-    println!("{}", hex::encode(hasher.result()));
+    let filename = args.skip(1).next().unwrap();
+
+    // Handle files that do not exist
+    if let Ok(filepath) = Path::new(&filename).canonicalize() {
+        let mut file = File::open(filepath).unwrap();
+        let mut buffer = [0; BUF_SIZE];
+        let mut hasher = MeowHasher::new();
+    
+        // Update hash from file using buffer
+        while file.read(&mut buffer[..]).unwrap() > 0 {
+            hasher.input(&buffer[..]);
+        }
+    
+        // Full 512-bit hash
+        let result = hasher.result();
+    
+        // Truncate to 160-bits (like `git hash-object`)
+        let hash = hex::encode(result).chars().take(40).collect::<String>();
+    
+        // Output hash to stdout
+        println!("{}", &hash);
+    
+    // Let the user know when a file cannot be found
+    } else {
+        println!("Error: file not found: {}", filename);
+    }
 }
