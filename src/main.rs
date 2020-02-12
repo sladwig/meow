ebddd037debd4eac2bba55d8034ed1209166b23a
Pebaz <pebazium@gmail.com>
2020-02-11 21:51:18 -0500

    read some data

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,29 @@ Fast file hasher in Rust using MeowHash.
 
 //extern crate digest;
 
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::BufReader;
+use std::io::prelude::*;
 use digest::*;
 use meowhash::*;
 
 fn main() {
     println!("Hello, world!");
 
-    let hasher = MeowHasher::new();
+    let mut hasher = MeowHasher::new();
 
-    //hasher.digest(3);
+    let data = vec![1, 2, 3, 4, 5];
+    hasher.input(&data);
 
-    hasher.input(3);
+	let result = hasher.result();
+
+    println!("{:?}", result);
+
+    println!("{}", hex::encode(result));
+
+    let file = OpenOptions::new().read(true).open(".gitignore").unwrap();
+    let mut buf_reader = BufReader::new(file);
+    let mut buffer = Vec::with_capacity(1024 * 1024);
+    buf_reader.read(&mut buffer).unwrap();
 }
