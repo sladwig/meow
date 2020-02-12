c292796531d82a7ce2b1bfbbd406d4f7cb2c7425
Pebaz <pebazium@gmail.com>
2020-02-12 11:42:19 -0500

    Working file hash

--- src/main.rs
+++ src/main.rs
@@ -4,42 +4,21 @@ pbz-hash
 Fast file hasher in Rust using MeowHash.
 */
 
-//extern crate digest;
-
-use std::fs::File;
 use std::fs::OpenOptions;
-use std::io::{BufReader, Empty};
 use std::io::prelude::*;
 use digest::*;
 use meowhash::*;
-use std::io;
-use std::io::prelude::*;
+
+const BUF_SIZE: usize = 1024 * 1024;
 
 fn main() {
-    println!("Hello, world!");
-
     let mut hasher = MeowHasher::new();
+    let mut file = OpenOptions::new().read(true).open(".gitignore").unwrap();    
+    let mut buffer = [0; BUF_SIZE];
 
-    //let data = vec![1, 2, 3, 4, 5];
-    //hasher.input(&data);
+    while file.read(&mut buffer[..]).unwrap() > 0 {
+        hasher.input(&buffer[..]);
+    }
 
-    //println!("{:?}", result);
-    //println!("{}", hex::encode(result));
-
-    let file = OpenOptions::new().read(true).open(".gitignore").unwrap();
-    let mut buf_reader = BufReader::new(file);
-
-
-    let buf_size = 1024 * 1024;
-    let mut buffer = Vec::with_capacity(buf_size);
-    loop {
-    	buf_reader.read(&mut buffer).unwrap();
-    	hasher.input(&buffer);
-    	if buf_reader.buffer().len() <= buf_size {
-    		break;
-    	}
-	}
-    let result = hasher.result();
-
-    println!("{}", hex::encode(result));
+    println!("{}", hex::encode(hasher.result()));
 }
